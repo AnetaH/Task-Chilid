0ba17ea6d1a0a1f9f42464ebe41e6c08cd0da396
Aneta Halz <aneta.halz@interia.pl>
2017-05-14 03:27:11 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,13 @@
+$('nav').affix({
+      offset: {
+        top: $('header').height()
+      }
+});
+
+$('nav').on('affix.bs.affix', function (){
+     $('body').css('margin-top', $('nav').height());
+});
+
+$('nav').on('affix-top.bs.affix', function (){
+     $('body').css('margin-top', 0);
+});
